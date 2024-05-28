2f7b3f68cda37141feaba0b76266109f6c80a6a4
ErickFSilva <erick_ferreira@outlook.com>
2024-05-28 16:42:18 -0300

    secao-01-para-quem-nao-entende-php, 28/05/2024(tarde)

---
+++ secao-01-para-quem-nao-entende-php/aula005-instrucao-ponto-e-virgula/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Ponto-e-vírgula é exigido no final de cada instrução em PHP
+$name = 'Alexandre';
+
+if ($name === 'Alexandre') {
+
+    echo 'é Alexandre mesmo.';
+}
+
+// A última linha de instrução não exige o ponto-e-vírgula, a tag do php estando fechada
+echo $name
+
+?>
---
+++ secao-01-para-quem-nao-entende-php/aula006-operadores-aritmeticos/index.php
@@ -0,0 +1,26 @@
+<?php
+
+// Operadores aritméticos em PHP
+
+$number1 = 100;
+$number2 = 20;
+
+// Soma:
+echo $number1 + $number2;
+
+// Subtração:
+echo $number1 - $number2;
+
+// Multiplicação:
+echo $number1 * $number2;
+
+// Divisão:
+echo $number1 / $number2;
+
+// Módulo:
+echo $number1 % $number2;
+
+
+// 
+$operacao = $number1 + $number2 * $number2;
+echo $operacao;
